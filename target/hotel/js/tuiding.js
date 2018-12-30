e624056376d7646a8ff730cd364c524cd2c2dae2
chgl16 <chenbetter1996@gmail.com>
2018-12-30 22:29:37 +0800

    xx

--- target/hotel/js/tuiding.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	var room = JSON.parse(data);
-	for(var i=0;i<room.length;i++){
-		$('#table-1 tbody').append("<tr><td>"+room[i].roomType+"</td><td><label>"+room[i].startDate+"</label> 至<br><label>"+room[i].endDate+"</label></td><td>"+room[i].price+"</td><td><input type='button' value='退订' class='bt'></td></tr>");
-	}
-});
